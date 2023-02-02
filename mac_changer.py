3083395657457663aad738543b5c25677615942b
gomous <mousumg911@gmail.com>
2023-02-02 23:24:17 +0530

    ninth commit

--- mac_changer.py
+++ mac_changer.py
@@ -24,5 +24,5 @@ def change_mac(interface, new_mac):
 
 options = get_arguments()
 # change_mac(options.interface, options.new_mac)
-ifconfig_result = (subprocess.check_output(["ifconfig", options.interface])).decode("utf-8")
-print(ifconfig_result)
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+print(ifconfig_result.decode("utf-8"))
