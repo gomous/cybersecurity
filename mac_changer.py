13869c91e90ab53c8c968a541bfffd4c91d95dcb
gomous <mousumg911@gmail.com>
2023-03-01 17:02:31 +0530

    11th commit

--- mac_changer.py
+++ mac_changer.py
@@ -4,16 +4,20 @@ import subprocess
 import optparse
 import re
 
+
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more info.")
+        parser.error(
+            "[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
         parser.error("[-] Please specify a new mac, use --help for more info.")
     return options
+
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -22,15 +26,21 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = (subprocess.check_output(
+        ["ifconfig", interface])).decode("utf-8")
+
+    print(ifconfig_result)
+
+    mac_address_search_result = re.search(
+        r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        print(mac_address_search_result.group(0))
+    else:
+        print("[-] Could not read MAC address.")
+
 
 options = get_arguments()
-# change_mac(options.interface, options.new_mac)
-ifconfig_result = (subprocess.check_output(["ifconfig", options.interface])).decode("utf-8")
-print(ifconfig_result)
-
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
-else:
-    print("[-] Could not read MAC address.")
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
